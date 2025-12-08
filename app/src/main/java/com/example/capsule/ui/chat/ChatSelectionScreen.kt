576c4517467719884617e1fc3170f3e843f04991
Mohamed Hany <mohamed23012559@gmail.com>
2025-12-08 21:16:52 +0200

    Update README.md

--- app/src/main/java/com/example/capsule/ui/chat/ChatSelectionScreen.kt
+++ app/src/main/java/com/example/capsule/ui/chat/ChatSelectionScreen.kt
@@ -3,12 +3,10 @@ package com.example.capsule.ui.chat
 import ChatHistoryViewModel
 import android.content.Intent
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -16,10 +14,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
